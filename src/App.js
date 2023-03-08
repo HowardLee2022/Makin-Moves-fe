678d15c8eecae1dc807d202ecc666f6db11292db
Jackie <jjac.lee@gmail.com>
2023-03-08 11:40:41 -0800

    trip page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes,Route} from "react-router-dom"
 import { Container } from 'react-bootstrap';
 import Home from "./pages/Home";
 import MyTrip from "./pages/mytrip";
+import AddTrip from "./pages/addtrip";
 
 import API from "./utils/API";
 
@@ -19,6 +20,7 @@ class App extends Component{
        <Routes>
          <Route path="/" element={<Home/>}/>
          <Route path="/mytrips" element={<MyTrip/>}/>
+         <Route path="/addtrip" element={<AddTrip/>}/>
        </Routes>
       </BrowserRouter>
       </Container>
