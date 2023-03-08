e7f170a0693c5901cfb1dc7dd74cf4024d8dc9f8
Jackie <jjac.lee@gmail.com>
2023-03-08 13:24:27 -0800

    nav

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 import Home from "./pages/Home";
 import MyTrip from "./pages/mytrip";
 import Days from "./pages/days";
-
+import Nav from "./components/Nav"
 import API from "./utils/API";
 
 class App extends Component{
@@ -17,6 +17,7 @@ class App extends Component{
     return (
       <Container>
           <BrowserRouter>
+          <Nav/>
        <Routes>
          <Route path="/" element={<Home/>}/>
          <Route path="/mytrips" element={<MyTrip/>}/>
