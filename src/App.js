51985ef91d0d6734940f421edda2ba28c8384d04
Nhan Duong <nhanduong@mjreseattle.com>
2023-03-09 11:03:17 -0800

    login

--- src/App.js
+++ src/App.js
@@ -4,14 +4,40 @@ import { Container } from 'react-bootstrap';
 import Home from "./pages/Home";
 import MyTrip from "./pages/mytrip";
 import Days from "./pages/days";
+import Modal from "./components/Modal";
+import Register from "./pages/Register";
 
-import API from "./utils/API";
+// import API from "./utils/API";
 
 class App extends Component{
   state = {
     isLoggedIn: false,
     username: ""
   }
+
+  // componentDidMount() {
+  //   this.logIn()
+  // }
+
+  // logIn = () => {
+  //   return API.getUser()
+  //   .then(user => {
+  //     this.setState({
+  //       isLoggedIn: user.data.loggedIn,
+  //       username: user.data.username
+  //     });
+  //   })
+  // }
+
+  // logOut = () => {
+  //   API.logout()
+  //     .then(res => {
+  //     this.setState({
+  //       isLoggedIn: false,
+  //       username: ""
+  //     });
+  //   })
+  // }
 
   render(){  
     return (
@@ -21,6 +47,8 @@ class App extends Component{
          <Route path="/" element={<Home/>}/>
          <Route path="/mytrips" element={<MyTrip/>}/>
          <Route path="/mytrips/:id" element = {<Days/>}/>
+         <Route path="/Modal" element = {<Modal/>}/>
+         <Route path="/Register" element = {<Register/>}/>
        </Routes>
       </BrowserRouter>
       </Container>
---
+++ src/components/Modal/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import LoginForm from "../LoginForm";
+
+function Modal(props) {
+  return (
+      <div className="modal fade" id="loginModal" tabIndex="-1" role="dialog" aria-labelledby="loginLabel" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title mx-auto text-center" id="loginLabel"><i className="fas fa-luggage-cart"></i> Welcome</h4>
+              {/* <button type="button" className="close ml-0 p-2" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button> */}
+            </div>
+            <div className="modal-body m-2">
+              <LoginForm login={props.login}/>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Modal;
