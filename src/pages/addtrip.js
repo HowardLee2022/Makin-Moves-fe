355a5112857083c01c1d2070d16577d614439c11
howard <howardnlee94@gmail.com>
2023-03-13 20:11:31 -0700

    styling

--- src/pages/addtrip.js
+++ src/pages/addtrip.js
@@ -35,30 +35,33 @@ function AddTrip(prop) {
 
 
     return (
+        <div class="container my-3">
+            <h1 class="text-center">Create a Trip</h1>
         <form onSubmit={handleSubmit}>
-            <div >
+            <div className="form-group">
                 <label >Title</label>
-                <input type="text"  placeholder="title" name="title" value={formData.title} onChange={handleChange} />
+                <input className="form-control" type="text"  placeholder="title" name="title" value={formData.title} onChange={handleChange} />
             </div>
-            <div >
+            <div className="form-group">
                 <label >start Date</label>
-                <input type="date"  placeholder="start date" name="start" onChange={handleChange}/>
+                <input  className="form-control" type="date"  placeholder="start date" name="start" onChange={handleChange}/>
             </div>
-            <div >
+            <div className="form-group">
                 <label >end date</label>
-                <input type="date"  placeholder="end date" name="end" onChange={handleChange}/>
+                <input className="form-control" type="date"  placeholder="end date" name="end" onChange={handleChange}/>
             </div>
-            <div >
+            <div className="form-group">
                 <label >cost</label>
-                <input type="number"  placeholder="cost" name="cost" onChange={handleChange}/>
+                <input className="form-control" type="number"  placeholder="cost" name="cost" onChange={handleChange}/>
             </div>
          
-            <div >
+            <div className="form-group">
                 <label>Description</label>
                 <textarea  rows="3" name="description" onChange={handleChange}></textarea>
             </div>
-            <button >Submit</button>
+            <button className="w-30 btn btn-primary btn-lg" >Submit</button>
         </form>
+        </div>
 
     )
 
