a04c01d700b19ddd636c8bb3444307539a79a898
nguyet33 <tdnguyet33@gmail.com>
2023-03-15 13:31:42 -0700

    bug fixing front end

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// const webpack = require('webpack');
+// const dotenv = require('dotenv');
+
+// dotenv.config();
+// require("dotenv").config();
+console.log(process.env.HARPERDB_URL);
+console.log(process.env.HARPERDB_PW);
+
+
+// function MyComponent() {
+//   const apiUrl = process.env.REACT_APP_API_URL;
+// }
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
